0777ba2f7296dbf5ea02535f724a140e16d18778
namoran <namoran@gmail.com>
2021-11-25 23:36:11 -0500

    renamed to openpyxl

--- comparenew_spi_to_rfc.py
+++ comparenew_spi_to_rfc.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import topenpyxl
+import openpyxl
 import streamlit as st
 
 tasks = ['TASK 1', 
@@ -167,18 +167,18 @@ in the Change Order were successfully transferred to the new SPI''')
 st.write('### Step 1. Upload the Excel version of the RFC')
 rfc = st.file_uploader("upload your rfc here",type='xlsm')
 if rfc != None:
-    wb_rfc = topenpyxl.load_workbook(rfc, read_only=True, data_only=True)
+    wb_rfc = openpyxl.load_workbook(rfc, read_only=True, data_only=True)
     ws_rfc = wb_rfc['RFC']
 st.write('### Step 2. Upload the most recent approved SPI')
 
 old_spi = st.file_uploader("Old SPI (the one you got from MFMP)",type='xlsm')
 if old_spi != None:
-    wb_old_spi = topenpyxl.load_workbook(old_spi, read_only=True, data_only=True)
+    wb_old_spi = openpyxl.load_workbook(old_spi, read_only=True, data_only=True)
     ws_old_spi = wb_old_spi['SOW Units']
 st.write('### Step 3. Upload the New SPI')
 new_spi = st.file_uploader("NEW SPI (The one you generated from the RFC)",type='xlsm')
 if new_spi != None:
-    wb_new_spi = topenpyxl.load_workbook(new_spi, read_only=True, data_only=True)
+    wb_new_spi = openpyxl.load_workbook(new_spi, read_only=True, data_only=True)
     ws_new_spi = wb_new_spi['SOW Units']
 
 
